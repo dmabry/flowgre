20cc620198b1a5226f69adae34e814970c3a676e
David Mabry <dmabry@ena.com>
2022-09-12 11:57:56 -0500

    Added single_test.go stubbed out for now.

---
+++ single/single_test.go
@@ -0,0 +1,11 @@
+// Use of this source code is governed by Apache License 2.0
+// that can be found in the LICENSE file.
+
+package single
+
+import "testing"
+
+// TestRun runs a test to verify functionality. TODO: Need to determine a sane test for this
+func TestRun(t *testing.T) {
+	// Perhaps create a listener on the given port and then process the packet to verify it is correct?
+}
