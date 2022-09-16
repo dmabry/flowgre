989d67cee3d0c220736b1d412ec97b790dcdd522
David Mabry <dmabry@users.noreply.github.com>
2022-09-15 21:23:59 -0500

    enhance drone build and build scripts to add tag to artifacts

--- scripts/build-multiplatform.sh
+++ scripts/build-multiplatform.sh
@@ -2,6 +2,9 @@
 # Use of this source code is governed by Apache License 2.0
 # that can be found in the LICENSE file.
 
+# Read first argument and pass it as tag otherwise set test
+[ -z "$1" ] && tag="test" || tag=$1
+
 oses=(windows darwin linux)
 archs=(amd64 arm64)
 
@@ -9,6 +12,6 @@ for os in ${oses[@]}
 do
   for arch in ${archs[@]}
   do
-        env GOOS=${os} GOARCH=${arch} go build -o flowgre_${os}_${arch}
+        env GOOS=${os} GOARCH=${arch} go build -o flowgre_${os}_${arch}_${tag}
   done
 done
