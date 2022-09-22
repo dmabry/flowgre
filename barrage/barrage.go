2b5b3bf65c9503593fd1ee4fae844121fbc08039
David Mabry <dmabry@users.noreply.github.com>
2022-09-22 14:30:23 -0500

    fixed typo in barrage.go comment.

--- barrage/barrage.go
+++ barrage/barrage.go
@@ -1,7 +1,7 @@
 // Use of this source code is governed by Apache License 2.0
 // that can be found in the LICENSE file.
 
-// Barrage is used to setup a continous stream of netflow packets towards a single collector
+// Barrage is used to setup a continuous stream of netflow packets towards a single collector
 
 package barrage
 
