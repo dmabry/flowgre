03ca2fb06d9ab301ad1311ce870271296a4303b4
David Mabry <dmabry@users.noreply.github.com>
2022-10-18 08:50:00 -0500

    record command now listens and prints packets off the wire

--- go.mod
+++ go.mod
@@ -19,8 +19,9 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/sys v0.0.0-20221010170243-090e33056c14 // indirect
 	golang.org/x/text v0.3.8 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- record/record.go
+++ record/record.go
@@ -7,11 +7,28 @@ package record
 
 import (
 	"log"
+	"net"
 	"os"
 )
 
 // Run Record
 func Run(ip string, port int, file string) {
+	//var buf bytes.Buffer
+	listenIP := net.ParseIP(ip)
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{IP: listenIP, Port: port})
+	if err != nil {
+		log.Fatalf("Listening on %s:%d failed! Got: %v", ip, port, err)
+	}
+	defer conn.Close()
 	log.Printf("Listening on %s:%d recording to %s.", ip, port, file)
+	for {
+		payload := make([]byte, 4096)
+		length, fromIP, perr := conn.ReadFromUDP(payload)
+		if err != nil {
+			log.Fatalf("Error reading packet from %s: %v", fromIP, perr)
+		}
+		payload = payload[:length]
+		log.Println(payload)
+	}
 	os.Exit(0)
 }
